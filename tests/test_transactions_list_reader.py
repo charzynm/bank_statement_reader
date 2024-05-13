b6f6f184dc58437b55a330803de83e432f3d522f
charzynm <charzynm@wp.pl>
2024-05-13 12:22:09 +0000

    Add test

--- tests/test_transactions_list_reader.py
+++ tests/test_transactions_list_reader.py
@@ -1,3 +1,4 @@
+from transactions_list_reader import FioTransactionsListReader
 from transactions_list_reader import CsobTransactionsListReader
 from transactions_list_reader import CSTransactionsListReader
 
@@ -10,3 +11,8 @@ def test_get_total_principal_csob():
     csobReader = CsobTransactionsListReader()
     csobReader.directory = "examples/csob"
     assert csobReader.get_total_principal() == 2700.00
+
+def test_get_total_principal_fio():
+    fioTransactionsListReader = FioTransactionsListReader()
+    fioTransactionsListReader.directory = "examples/fio"
+    assert fioTransactionsListReader.get_total_principal() == 800.00
